6feea5a89dec4949d93264aa5079b8b87a7e4382
javired10 <pmarcac@est.ups.edu.ec>
2026-05-17 15:37:00 -0500

    base de codigo

---
+++ models/Producto.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    nombre: { type: String, required: true },
+    precio: { type: Number, required: true },
+    descripcion: { type: String, required: true },
+    imagen: { type: String, required: true }
+});
+
+module.exports = mongoose.model('Producto', productSchema);
